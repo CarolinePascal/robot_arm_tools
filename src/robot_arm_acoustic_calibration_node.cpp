5d4cb3a1bc14bb3b5ea81c4f0b4b0eb99bf5754c
CarolinePascal <caroline8.pascal@gmail.com>
2021-05-19 14:01:40 +0200

    Adding new nodes

---
+++ src/robot_arm_acoustic_calibration_node.cpp
@@ -0,0 +1,103 @@
+#include <ros/ros.h>
+#include <robot_arm_tools/Robot.h>
+#include <robot_arm_tools/RobotTrajectories.h>
+#include <robot_arm_tools/RobotVisualTools.h>
+
+#include <std_srvs/Empty.h>
+
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+int main(int argc, char **argv)
+{
+    //ROS node initialisation
+    ros::init(argc, argv, "robot_arm_acoustic_calibration_node");  
+    ros::AsyncSpinner spinner(0);
+    spinner.start();
+    ros::WallDuration(1.0).sleep();
+
+    //Command line arguments handling
+    if(argc < 3)
+    {
+        throw std::invalid_argument("MISSING CMD LINE ARGUMENT FOR acoustic_micro_node !");
+        return(1);
+    }
+
+    //Robot initialisation TODO More generic approach
+    Robot robot("panda_arm", argv[1], "panda_" + std::string(argv[1]));
+
+    //Get ROS service calibration server name and create client
+    ros::NodeHandle n;
+    ros::ServiceClient calibrationClient = n.serviceClient<std_srvs::Empty>(argv[2]);
+
+    //Robot visual tools initialisation
+    RobotVisualTools visualTools;
+    visualTools.setupU2IS();
+
+    //Move the robot to its initial configuration
+    robot.init();
+
+    //Tf listenner initialisation
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    //Switch the robot to manual control mode (user action required)
+    do 
+    {
+        ROS_INFO("Switch the robot to manual control mode, and move the tool to its reference position - Press enter to continue");
+    } while (std::cin.get() != '\n');
+
+    std_srvs::Empty request;
+
+    if(calibrationClient.call(request))
+    {
+        ROS_INFO("Calibration - done !");
+    }
+
+    else
+    {
+        throw std::runtime_error("ERROR DURING CALIBRATION !");
+        ros::waitForShutdown();
+        return 1;
+    }
+
+    //Display reference position
+
+    geometry_msgs::Transform transform;
+    try
+    {
+        transform = tfBuffer.lookupTransform("world", std::string("panda_")+std::string(argv[1]), ros::Time(0), ros::Duration(5.0)).transform;
+    } 
+    catch (tf2::TransformException &ex) 
+    {
+        throw std::runtime_error("CANNOT RETRIVE SEEKED TRANSFORM !");
+    }
+
+    tf2::Quaternion quaternion;
+    tf2::fromMsg(transform.rotation,quaternion);
+    double roll, pitch, yaw;
+    tf2::Matrix3x3(quaternion).getRPY(roll, pitch, yaw);
+
+    ROS_INFO("Reference position : \n X : %f \n Y : %f \n Z : %f \n RX : %f \n RY : %f \n RZ : %f",transform.translation.x,transform.translation.y,transform.translation.z,roll,pitch,yaw);
+
+    //Save reference position
+    
+    std::string yamlFile = ros::package::getPath("robot_arm_acoustic")+"/config/AcquisitionParameters.yaml";
+
+    YAML::Node config = YAML::LoadFile(yamlFile);
+
+    if (config["poseReference"]) 
+    {
+        config.remove("poseReference");
+        config["poseReference"].push_back(transform.translation.x);
+        config["poseReference"].push_back(transform.translation.y);
+        config["poseReference"].push_back(transform.translation.z);
+        config["poseReference"].push_back(roll);
+        config["poseReference"].push_back(pitch);
+        config["poseReference"].push_back(yaw);
+    }
+
+    ros::waitForShutdown();
+    return 0;
+}
+
---
+++ src/robot_arm_acoustic_measurement_node.cpp
@@ -0,0 +1,154 @@
+#include <ros/ros.h>
+#include <robot_arm_tools/Robot.h>
+#include <robot_arm_tools/RobotTrajectories.h>
+#include <robot_arm_tools/RobotVisualTools.h>
+
+#include <std_srvs/Empty.h>
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <iostream>
+#include <fstream>
+
+#include <cmath>
+
+int main(int argc, char **argv)
+{
+    //ROS node initialisation
+    ros::init(argc, argv, "robot_arm_acoustic_measurement_node");  
+    ros::AsyncSpinner spinner(0);
+    spinner.start();
+    ros::WallDuration(1.0).sleep();
+
+    //Command line arguments handling
+    if(argc < 3)
+    {
+        throw std::invalid_argument("MISSING CMD LINE ARGUMENT FOR acoustic_micro_node !");
+        return(1);
+    }
+
+    //Robot initialisation TODO More generic approach
+    Robot robot("panda_arm", argv[1], "panda_" + std::string(argv[1]));
+
+    //Get ROS service measurement server name and create client
+    ros::NodeHandle n;
+    ros::ServiceClient measurementClient = n.serviceClient<std_srvs::Empty>(argv[2]);
+
+    //Robot visual tools initialisation
+    RobotVisualTools visualTools;
+    visualTools.setupU2IS();
+
+    //Move the robot to its initial configuration
+    robot.init();
+
+    //Load object geometry
+    std::vector<double> poseObject;
+    double radiusObject;
+    double radiusTrajectory;
+
+    ROS_INFO("Getting acquisition parameters");
+
+    n.getParam("poseObject",poseObject);
+    n.getParam("radiusObject",radiusObject);
+    n.getParam("radiusTrajectory",radiusTrajectory);
+
+    geometry_msgs::Pose centerPose;
+    centerPose.position.x = poseObject[0];
+    centerPose.position.y = poseObject[1];
+    centerPose.position.z = poseObject[2];
+    
+    if(radiusObject != 0)
+    {
+        visualTools.addSphere("collisionSphere", radiusObject, centerPose, true);
+    }
+
+    //Compute spherical scanning trajectory points
+    tf2::Quaternion leftQuaternion, rightQuaternion;
+    leftQuaternion.setRPY(M_PI/2,-M_PI/2 + atan2(centerPose.position.y,centerPose.position.x),0);
+    rightQuaternion.setRPY(-M_PI/2,M_PI/2 - atan2(centerPose.position.y,centerPose.position.x),0);
+
+    int N=10;   //Waypoints number
+    std::vector<geometry_msgs::Pose> waypoints;
+
+    double dinclination = M_PI / (N-1);
+
+    for(int i = 0; i < N; i++)
+    {
+        sphericInclinationTrajectory(radiusTrajectory, i*dinclination, 0, 2*M_PI, 0, centerPose, N*(i > 0 ? 1 : 0), waypoints); //1+round((N-1)*sin(i*dinclination))   
+    }
+    
+    //Create .csv file for positions recording - Folder was already created in MicroService node
+    std::ofstream myfile;
+    myfile.open("/tmp/AcousticMeasurement/Positions.csv");
+
+    //Acquisition loop
+    for(int i = 0; i < waypoints.size(); i++)
+    {        
+        //Pre-processing
+        if(waypoints[i].position.x*centerPose.position.y/centerPose.position.y > waypoints[i].position.y)
+        {
+            waypoints[i].orientation = tf2::toMsg(rightQuaternion); 
+        }
+        else
+        {
+            waypoints[i].orientation = tf2::toMsg(leftQuaternion); 
+        }
+
+        ROS_INFO("Waypoint %i out of %i", i+1, (int)waypoints.size());
+        //std::cout<<waypoints[i]<<std::endl;
+
+        visualTools.addAxis("waypoint",waypoints[i]);
+        
+        try
+        {
+            robot.goToTarget(waypoints[i],false);
+            visualTools.deleteMarker("waypoint");
+        }
+        catch(const std::runtime_error& e)
+        {
+            ROS_WARN("Skipping unreachable waypoint");
+            visualTools.deleteMarker("waypoint");
+            continue;
+        }
+
+        ros::WallDuration(1.0).sleep();
+
+        if(measurementClient.call(request))
+        {
+            ROS_INFO("Measurement - done !");
+        }
+
+        else
+        {
+            throw std::runtime_error("ERROR DURING Measurement !");
+            ros::waitForShutdown();
+            return 1;
+        }
+
+        tf2::Quaternion quaternion;
+        tf2::fromMsg(waypoints[i].orientation,quaternion);
+        double roll, pitch, yaw;
+        tf2::Matrix3x3(quaternion).getRPY(roll, pitch, yaw);
+
+        myfile << i+1;
+        myfile << ",";
+        myfile << waypoints[i].position.x;
+        myfile << ",";
+        myfile << waypoints[i].position.y;
+        myfile << ",";
+        myfile << waypoints[i].position.z;
+        myfile << ",";
+        myfile << roll;
+        myfile << ",";
+        myfile << pitch;
+        myfile << ",";
+        myfile << yaw;
+        myfile << "\n";
+    }
+
+    //Close .csv file and shut down ROS node
+    myfile.close();
+    robot.init();   
+    ros::waitForShutdown();
+    return 0;
+}
