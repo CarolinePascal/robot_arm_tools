6069b5d119e23ea0241fa2f26a875b0f8c9cb7ec
Pascal Caroline <caroline8.pascal@gmail.com>
2021-12-16 23:57:49 +0100

    Creating FreeFem running python scripts, new implementation of the spheric planar cut parameters handling

--- post_processing/RunDerivativeComparaison.py
+++ post_processing/RunDerivativeComparaison.py
@@ -7,38 +7,25 @@ from ThickSphereMesh import createMesh
 
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
-
-folderList = np.array(glob.glob("Baseline*"))
-
-maxFolder = 0
-for folder in folderList:
-    folderIndex = int(folder[-1])
-    if(folderIndex > maxFolder):
-        maxFolder = folderIndex
-
-os.mkdir("Baseline_"+str(maxFolder+1))
 
 Rmin = 0.2
-Rmax = 0.21
-R = 1.0
+Ntheta = 10
+Nphi = 10
 
 DeltaR = np.linspace(0.001,1,10)
-Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),5),0).astype(int)
-Vertices = np.round(np.logspace(1,2,5),0).astype(int)
+Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),10),0).astype(int)
 
 fig, ax = plt.subplots()
 
-for vertex in Vertices:
+for frequency in Frequencies:
     Error = []
-    for frequency in Frequencies:
-        Ntheta = vertex
-        Nphi = vertex
+    for delta in DeltaR:
+        Rmax = np.round(Rmin + delta,5)
 
         if(not os.path.isfile("meshes/thick_sphere/ST_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
             createMesh(Ntheta,Nphi,Rmin,Rmax)
 
-        bashCommand = "FreeFem++ AcousticDipole.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax) + " -R " + str(R) + " -folderName Baseline_" + str(maxFolder+1) 
+        bashCommand = "FreeFem++ DerivativeComparaison.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
@@ -47,7 +34,7 @@ for vertex in Vertices:
         error = float(msg[-msg[::-1].find('\n\n'):-1])
         Error.append(error)
     
-    ax.plot(Vertices,np.log10(Error),label=str(frequency) + "Hz")
+    ax.plot(DeltaR,np.log10(Error),label=str(frequency) + "Hz")
 
 
 ax.set_xlabel("Radius delta (m)")
--- post_processing/plotSphericPlaneCut.py
+++ post_processing/plotSphericPlaneCut.py
@@ -3,124 +3,140 @@ import numpy as np
 import csv
 
 import glob
+import os
+import sys
 
 from copy import deepcopy
 
 c = 343.4
 
-directoryList = np.array(glob.glob("outputs*"))
+fileList = np.array(glob.glob("output*"))
+parametersList = []
 
-P = np.zeros((len(directoryList),len(directoryList[0].split("_")) - 1))
+try:
+    with open("parameters.txt") as parametersFile:
+        parametersList = parametersFile.read().splitlines()
+except:
+    for i in range(len(fileList[0].split("_")) - 1):
+        parametersList.append("parameter"+str(i+1))
 
-for i,directory in enumerate(directoryList):
-    print(directory)
-    P[i] = np.array([float(l) for l in directory.split("_")[1:]])
+parametersList = np.array(parametersList)
 
-subPList = [P[P[:,0].argsort()]]
+P = np.empty((len(fileList),len(parametersList)))
 
-for j in range(len(P[0])-1):
-    newSubPList = []
-    for subP in subPList:
-        newSubPList+=[subP[l] for l in (np.where(subP[:,j] == k)[0] for k in np.unique(subP[:,j]))]
+for i,file in enumerate(fileList):
+    P[i] = np.array([float(l) for l in os.path.splitext(file)[0].split("_")[1:]])
 
-    for m,newSubP in enumerate(newSubPList):
-        tmp = newSubP[newSubP[:,j+1].argsort()]
-        newSubPList[m] = tmp
-
-    subPList = deepcopy(newSubPList)
-
-newP = np.concatenate(subPList)
-I = np.where((P == newP[:,None]).all(-1))[1]
-P = P[I]
+parameterIndex = np.argwhere(parametersList == sys.argv[1])[0][0]
 
 print(P)
+tempColumn = deepcopy(P[:,0])
+P[:,0] = P[:,parameterIndex]
+P[:,parameterIndex] = tempColumn
+print(P)
+
+print(parametersList)
+tempName = parametersList[0]
+parametersList[0] = parametersList[parameterIndex]
+parametersList[parameterIndex] = tempName
+print(parametersList)
+
+sortedIndices = P[:, 0].argsort()
+P = P[sortedIndices]
+fileList = fileList[sortedIndices]
+
+print(P)
+print(fileList)
+
+parameterValues = np.unique(P[:,0])
+
+print(parameterValues)
+
+configurationsNumber = len(P)//len(parameterValues)
+configurationsList = P[:configurationsNumber,1:]
+
+print(configurationsList)
+
+plotList = np.empty((configurationsNumber,len(parameterValues)))
+
+for i,file in enumerate(fileList):
+    configurationIndex = np.where((P[i,1:]==configurationsList).all(axis=1))[0][0]
+
+    X = []
+    Y = []
+    Z = []
+    AmpSomme = []
+    AmpAnalytique = []
+
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+
+        for row in reader:
+            X.append(float(row[0]))
+            Y.append(float(row[1]))
+            Z.append(float(row[2]))
+    
+            AmpSomme.append(20*np.log10(np.abs(np.complex(float(row[3]),float(row[4])))/20e-6))
+            AmpAnalytique.append(20*np.log10(np.abs(np.complex(float(row[5]),float(row[6])))/20e-6))
+
+    #X0 = np.argmax(np.abs(np.array(X)))
+
+    AmpSomme = np.array(AmpSomme)
+    AmpAnalytique = np.array(AmpAnalytique)
+
+    #AmpAnalytique = np.roll(AmpAnalytique,X0)
+    AmpAnalytique = np.append(AmpAnalytique,AmpAnalytique[0])
+    #AmpSomme = np.roll(AmpSomme,X0)
+    AmpSomme = np.append(AmpSomme,AmpSomme[0])
+
+    plotList[configurationIndex][i//configurationsNumber] = np.average(np.abs(AmpSomme-AmpAnalytique))
+
+    #TH = np.arange(0,2*np.pi,2*np.pi/len(X))
+    #TH = np.append(TH,2*np.pi)
+#
+    #fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    #ax.plot(TH,AmpAnalytique,label="Analytical solution (dB)")
+    #ax.plot(TH,AmpSomme,label="Numerical solution (dB)")
+#
+    #label = "Acoustic pressure field computed for \n"
+    #for j,name in enumerate(parametersList[1:]):
+    #    if(len(np.unique(configurationsList[:,j])) > 1):
+    #        label += name + " = " + str(P[i,j+1]) + "\n"
+    #label = label[:-1]
+#
+    #ax.set_title(label, x=-0.25, y=0.25)
+#
+    #maxAmp = max(max(AmpSomme),max(AmpAnalytique))*1.1
+#
+    #ax.annotate('x', xy=(np.pi/40,maxAmp), xycoords='data', annotation_clip=False, size = 12)
+    #ax.annotate('y', xy=(np.pi/2 - np.pi/40,maxAmp), xycoords='data', annotation_clip=False, size = 12)
+#
+    #plt.legend()
+    #plt.show()
 
 figLog, axLog = plt.subplots()
 figLin, axLin = plt.subplots()
 
-for j,directory in enumerate(directoryList[I]):
-    kd = P[j,1]*2*np.pi*P[j,0]/c
+for i,configuration in enumerate(configurationsList):
+    label = ""
+    for j,name in enumerate(parametersList[1:]):
+        if(len(np.unique(configurationsList[:,j])) > 1):
+            label += name + " = " + str(P[i,j+1]) + "\n"
+    label = label[:-1]
+    axLin.plot(parameterValues,plotList[i],label=label)
+    axLog.plot(parameterValues,np.log10(plotList[i]),label=label)
 
-    fileList = np.array(glob.glob(directory + "/data_output*.txt"))
-    V = []
-    E = []
-
-    for file in fileList:
-        L = file.split('.')[:-1]
-        file = ""
-        for item in L:
-            file = file+item
-        V.append(int(file.split('.')[0].split('_')[-1]))
-
-    Iv = np.argsort(V)
-    V = np.sort(V)
-
-    for i,file in enumerate(fileList[Iv]):
-        print("Number of vertices : " + str(V[i]))
-
-        X = []
-        Y = []
-        Z = []
-        AmpSomme = []
-        AmpAnalytique = []
-
-        with open(file, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=';', quotechar='|')
-
-            for row in reader:
-                X.append(float(row[0]))
-                Y.append(float(row[1]))
-                Z.append(float(row[2]))
-        
-                AmpSomme.append(20*np.log10(np.abs(np.complex(float(row[3]),float(row[4])))/20e-6))
-                AmpAnalytique.append(20*np.log10(np.abs(np.complex(float(row[5]),float(row[6])))/20e-6))
-
-        #X0 = np.argmax(np.abs(np.array(X)))
-
-        AmpSomme = np.array(AmpSomme)
-        AmpAnalytique = np.array(AmpAnalytique)
-
-        #AmpAnalytique = np.roll(AmpAnalytique,X0)
-        AmpAnalytique = np.append(AmpAnalytique,AmpAnalytique[0])
-        #AmpSomme = np.roll(AmpSomme,X0)
-        AmpSomme = np.append(AmpSomme,AmpSomme[0])
-
-        E.append(np.average(np.abs(AmpSomme-AmpAnalytique)))
-        print("Erreur : " + str(E[-1]))
-
-        #if(i!=len(fileList)-1):
-        #    continue
-
-        """TH = np.arange(0,2*np.pi,2*np.pi/len(X))
-        TH = np.append(TH,2*np.pi)
-
-        fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-        ax.plot(TH,AmpAnalytique,label="Analytical solution (dB)")
-        ax.plot(TH,AmpSomme,label="Numerical solution (dB)")
-
-        ax.set_title("Acoustic pressure field computed for " + str(V[i]) + " vertices\n" + "Frequency : " + str(P[j,0]) + "\n" + "Delta radius : " + str(P[j,2]) + "\n" + "Far field radius : " + str(P[j,3]))
-
-        maxAmp = max(max(AmpSomme),max(AmpAnalytique))*1.1
-
-        ax.annotate('x', xy=(np.pi/40,maxAmp), xycoords='data', annotation_clip=False, size = 12)
-        ax.annotate('y', xy=(np.pi/2 - np.pi/40,maxAmp), xycoords='data', annotation_clip=False, size = 12)
-
-        plt.legend()
-        plt.show()"""
-
-    axLin.plot(V,E,label="Frequency : " + str(P[j,0]) + "\n" + "Delta radius : " + str(P[j,2]) + "\n" + "F.F. radius : " + str(P[j,3]))
-    axLog.plot(V,np.log(E),label="Frequency : " + str(P[j,0]) + "\n" + "Delta radius : " + str(P[j,2]) + "\n" + "F.F. radius : " + str(P[j,3]))
-
-axLog.set_xlabel("Number of vertices")
+axLog.set_xlabel(sys.argv[1])
 axLog.set_ylabel("log(Average error)")
 axLog.set_xscale('log')  
-axLog.set_title("Average error depending on the number of vertices")    
+axLog.set_title("log(Average error)")    
 
 axLog.legend()
 
-axLin.set_xlabel("Number of vertices")
+axLin.set_xlabel(sys.argv[1])
 axLin.set_ylabel("Average error")
-axLin.set_title("Average error depending on the number of vertices")   
+axLin.set_title("Average error")   
 
 axLin.legend()
+
 plt.show()
