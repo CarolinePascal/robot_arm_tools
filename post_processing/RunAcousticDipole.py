3ee964ccf1e9b78364a32ce14b1db293f666e4a7
CarolinePascal <caroline8.pascal@gmail.com>
2021-12-17 18:15:40 +0100

    Renaming meshes, adding mkdir, adding new parameters range

--- post_processing/RunAcousticDipole.py
+++ post_processing/RunAcousticDipole.py
@@ -20,14 +20,11 @@ for folder in folderList:
 os.mkdir("Baseline_"+str(maxFolder+1))
 
 Rmin = 0.2
-Rmax = 0.21
+Rmax = 0.3
 R = 1.0
 
-DeltaR = np.linspace(0.001,1,10)
-Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),5),0).astype(int)
-Vertices = np.round(np.logspace(1,2,5),0).astype(int)
-
-fig, ax = plt.subplots()
+Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),10),0).astype(int)
+Vertices = np.round(np.logspace(1,2,10),0).astype(int)
 
 for vertex in Vertices:
     Error = []
@@ -35,24 +32,12 @@ for vertex in Vertices:
         Ntheta = vertex
         Nphi = vertex
 
-        if(not os.path.isfile("meshes/thick_sphere/ST_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
+        if(not os.path.isfile("meshes/thick_sphere/TS_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
             createMesh(Ntheta,Nphi,Rmin,Rmax)
 
         bashCommand = "FreeFem++ AcousticDipole.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax) + " -R " + str(R) + " -folderName Baseline_" + str(maxFolder+1) 
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
-        msg = output.decode()
-        #print(msg)
-        error = float(msg[-msg[::-1].find('\n\n'):-1])
-        Error.append(error)
-    
-    ax.plot(Vertices,np.log10(Error),label=str(frequency) + "Hz")
-
-
-ax.set_xlabel("Radius delta (m)")
-ax.set_xscale('log')  
-ax.set_ylabel("log(Average error)")
-ax.set_title("Average error depending on the radius delta")    
-ax.legend()
-plt.show()
+        #DEBUG
+        #print(output.decode())
--- post_processing/RunDerivativeComparaison.py
+++ post_processing/RunDerivativeComparaison.py
@@ -12,34 +12,25 @@ Rmin = 0.2
 Ntheta = 10
 Nphi = 10
 
+if(not os.path.isdir("Derivative/")):
+    os.mkdir("Derivative/")
+
 DeltaR = np.linspace(0.001,1,10)
 Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),10),0).astype(int)
-
-fig, ax = plt.subplots()
 
 for frequency in Frequencies:
     Error = []
     for delta in DeltaR:
         Rmax = np.round(Rmin + delta,5)
 
-        if(not os.path.isfile("meshes/thick_sphere/ST_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
+        if(not os.path.isfile("meshes/thick_sphere/TS_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
             createMesh(Ntheta,Nphi,Rmin,Rmax)
 
         bashCommand = "FreeFem++ DerivativeComparaison.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
-        msg = output.decode()
-        #print(msg)
-        error = float(msg[-msg[::-1].find('\n\n'):-1])
-        Error.append(error)
+        #DEBUG
+        #print(output.decode())
+
     
-    ax.plot(DeltaR,np.log10(Error),label=str(frequency) + "Hz")
-
-
-ax.set_xlabel("Radius delta (m)")
-ax.set_xscale('log')  
-ax.set_ylabel("log(Average error)")
-ax.set_title("Average error depending on the radius delta")    
-ax.legend()
-plt.show()
