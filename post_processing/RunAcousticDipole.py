56e8a9f7f687f61dba470e1783a14c6ce3a266a7
CarolinePascal <caroline8.pascal@gmail.com>
2022-01-07 09:59:18 +0100

    Adding loops for each tested parameters

--- post_processing/RunAcousticDipole.py
+++ post_processing/RunAcousticDipole.py
@@ -10,37 +10,45 @@ import matplotlib.pyplot as plt
 import glob
 
 #TODO Name folder according to tested parameters ?
-folderList = np.array(glob.glob("Baseline*"))
+folderList = np.array(glob.glob("Baseline*/"))
 
 maxFolder = 0
 for folder in folderList:
-    folderIndex = int(folder[-1])
+    folderIndex = int(folder[-2])
     if(folderIndex > maxFolder):
         maxFolder = folderIndex
 
 os.mkdir("Baseline_"+str(maxFolder+1))
 
 Rmin = 0.2
-Rmax = 0.3
 R = 1.0
 
-Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),10),0).astype(int)
-Vertices = np.round(np.logspace(1,2,10),0).astype(int)
+#DeltaR = np.round(np.logspace(-3,0,10),3)
+#Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),10),0)
+#Vertices = np.round(np.logspace(1,2,10),0).astype(int)
+
+Vertices = [10]
+Frequencies = [20,200,2000,20000]
+DeltaR = np.round(np.logspace(-3,0,50),5)
 
 for vertex in Vertices:
     print("Vertices : " + str(vertex))
-    Error = []
+    Ntheta = vertex
+    Nphi = vertex
+
     for frequency in Frequencies:
         print("Frequency : " + str(frequency))
-        Ntheta = vertex
-        Nphi = vertex
 
-        if(not os.path.isfile("meshes/thick_sphere/TS_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
-            createMesh(Ntheta,Nphi,Rmin,Rmax)
+        for delta in DeltaR:
+            print("DeltaR : " + str(delta))
+            Rmax = np.round(Rmin + delta,5)
 
-        bashCommand = "FreeFem++ AcousticDipole.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax) + " -R " + str(R) + " -folderName Baseline_" + str(maxFolder+1) 
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
+            if(not os.path.isfile("meshes/thick_sphere/TS_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
+                createMesh(Ntheta,Nphi,Rmin,Rmax)
 
-        #DEBUG
-        #print(output.decode())
+            bashCommand = "FreeFem++ AcousticDipole.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax) + " -R " + str(R) + " -folderName Baseline_" + str(maxFolder+1) 
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+            #DEBUG
+            #print(output.decode())
--- post_processing/RunDerivativeComparaison.py
+++ post_processing/RunDerivativeComparaison.py
@@ -8,30 +8,45 @@ from ThickSphereMesh import createMesh
 import numpy as np
 import matplotlib.pyplot as plt
 
-Rmin = 0.2
-Ntheta = 10
-Nphi = 10
+import glob
 
 #TODO Name folder according to tested parameters ?
-if(not os.path.isdir("Derivative/")):
-    os.mkdir("Derivative/")
+folderList = np.array(glob.glob("Derivative*/"))
 
-DeltaR = np.linspace(0.001,1,10)
-Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),10),0).astype(int)
+maxFolder = 0
+for folder in folderList:
+    folderIndex = int(folder[-2])
+    if(folderIndex > maxFolder):
+        maxFolder = folderIndex
 
-for frequency in Frequencies:
-    Error = []
-    for delta in DeltaR:
-        Rmax = np.round(Rmin + delta,5)
+os.mkdir("Derivative_"+str(maxFolder+1))
 
-        if(not os.path.isfile("meshes/thick_sphere/TS_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
-            createMesh(Ntheta,Nphi,Rmin,Rmax)
+Rmin = 0.2
 
-        bashCommand = "FreeFem++ DerivativeComparaison.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax)
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
+DeltaR = np.round(np.logspace(-3,0,10),3)
+Frequencies = np.round(np.logspace(np.log10(20),np.log10(20000),10),0)
+Vertices = np.round(np.logspace(1,2,10),0).astype(int)
 
-        #DEBUG
-        #print(output.decode())
+for vertex in Vertices:
+    print("Vertices : " + str(vertex))
+    Ntheta = vertex
+    Nphi = vertex
+
+    for frequency in Frequencies:
+        print("Frequency : " + str(frequency))
+
+        for delta in DeltaR:
+            print("DeltaR : " + str(delta))
+            Rmax = np.round(Rmin + delta,5)
+
+            if(not os.path.isfile("meshes/thick_sphere/TS_"+str(Ntheta)+"_"+str(Rmin)+"_"+str(Rmax)+".mesh")):
+                createMesh(Ntheta,Nphi,Rmin,Rmax)
+
+            bashCommand = "FreeFem++ DerivativeComparaison.edp -frequency " + str(frequency) + " -angularVerticesNumber " +str(Ntheta) + " -Rmin " + str(Rmin) + " -Rmax " + str(Rmax) + " -folderName Derivative_" + str(maxFolder+1) 
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+            #DEBUG
+            #print(output.decode())
 
     
