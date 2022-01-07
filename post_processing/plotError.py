Adding fixed parameters and parameters units

post_processing/plotError.py
@@ -7,35 +7,49 @@ import os
 import sys
 
 from functools import reduce
+import scipy as sp
 
+#Post-processing functions
 postProcessingFunctions = {}
 postProcessingFunctions["db"] = lambda Re,Im : 20*np.log10(np.abs(np.complex(Re,Im))/20e-6)
 postProcessingFunctions["abs"] = lambda Re,Im : np.abs(np.complex(Re,Im))
 postProcessingFunctions["id"] = lambda Re,Im : np.complex(Re,Im)
 
-def plotError(postProcessingError = "dB"):
+def plotError(postProcessingError):
 
     #Get all output files
     fileList = np.array(glob.glob("output*"))
+    
+    #Get all parameters names, or define them if none given
     parametersList = []
+    parametersUnits = []
 
-    #Get all parameters names, or define them
     try:
         with open("parameters.txt") as parametersFile:
-            parametersList = parametersFile.read().splitlines()
+            lines = parametersFile.read().splitlines()
+            for line in lines:
+                try:
+                    name,unit = line.split(' ')
+                except:
+                    name = line
+                    unit=""
+                parametersList.append(name)
+                parametersUnits.append(unit)
     except:
         for i in range(len(fileList[0].split("_")) - 1):
             parametersList.append("parameter"+str(i+1))
+            parametersUnits.append("")
 
     parametersList = np.array(parametersList)
+    parametersUnits = np.array(parametersUnits)
 
-    #Create files/parameters matrix
+    #Create the output files / parameters configurations matrix
     P = np.empty((len(fileList),len(parametersList)))
 
     for i,file in enumerate(fileList):
         P[i] = np.array([float(l) for l in os.path.splitext(file)[0].split("_")[1:]])
 
-    #Put the studied parameter in the first column
+    #Put the studied parameter in the first position in each parameters configurations
     try:
         parameter = sys.argv[1]
     except:
@@ -44,13 +58,14 @@ def plotError(postProcessingError = "dB"):
     parameterIndex = np.argwhere(parametersList == parameter)[0][0]
     P[:,[0,parameterIndex]] = P[:,[parameterIndex,0]]
     parametersList[[0,parameterIndex]] = parametersList[[parameterIndex,0]]
+    parametersUnits[[0,parameterIndex]] = parametersUnits[[parameterIndex,0]]
 
-    #Sort files according to the studied parameter values
+    #Sort the output files according to the studied parameter values
     sortedIndices = P[:, 0].argsort()
     P = P[sortedIndices]
     fileList = fileList[sortedIndices]
 
-    #Get studied parameter values
+    #Get all possible values for the studied parameter
     parameterValues = np.unique(P[:,0])
     lastIndex = []
     for value in parameterValues:
@@ -58,11 +73,26 @@ def plotError(postProcessingError = "dB"):
 
     splittedP = np.split(P,lastIndex)[:-1]
 
+    #Get the parameters configurations defined for all possible values of the studied parameter (an not just some of them)
     interestConfigurations = np.unique(P[:,1:],axis=0)
 
     for configurations in splittedP:
         configurations = configurations[:,1:]
         interestConfigurations = interestConfigurations[(interestConfigurations == configurations[:, None]).any(axis=0).all(axis=-1)]
+
+    #Any fixed parameter ? 
+    flag = input("Any fixed parameter ? y/n")
+    tmpParametersList = parametersList[1:]
+
+    while(flag != "n"):
+        tmpParameter = input("What parameter ? " + str(tmpParametersList))
+        tmpParameterIndex = np.argwhere(parametersList[1:] == tmpParameter)[0][0]
+        tmpValue = list(input("what values ? " + str(np.unique(interestConfigurations[:,tmpParameterIndex])) + " (" + parametersUnits[1:][tmpParameterIndex] + ") ").split(' '))
+        tmpValue = [float(item) for item in tmpValue]
+
+        interestConfigurations = interestConfigurations[np.argwhere(np.isin(interestConfigurations[:,tmpParameterIndex], tmpValue))[:,0]]
+        tmpParametersList = np.delete(tmpParametersList,np.argwhere(tmpParametersList == tmpParameter)[0][0])
+        flag = input("Any fixed parameter ? y/n")
 
     plotList = np.empty((len(interestConfigurations),len(parameterValues)))
 
@@ -101,32 +131,50 @@ def plotError(postProcessingError = "dB"):
         #AmpSomme = np.roll(AmpSomme,X0)
         AmpSomme = np.append(AmpSomme,AmpSomme[0])
 
-        plotList[configurationIndex][parameterValueIndex] = np.average(np.abs(AmpSomme-AmpAnalytique))
+        #Removing infinite values
+        IndexInf = np.argwhere(np.isinf(AmpAnalytique))
+        AmpAnalytique = np.delete(AmpAnalytique,IndexInf)
+        AmpSomme = np.delete(AmpSomme,IndexInf)
 
+        plotList[configurationIndex][parameterValueIndex] = np.sqrt(np.average((AmpSomme - AmpAnalytique)**2))
+        
     figLog, axLog = plt.subplots()
     figLin, axLin = plt.subplots()
 
     cmap = plt.cm.get_cmap('gist_rainbow', len(interestConfigurations))
 
+    title = "Error computed with : \n" 
+
+    for j,name in enumerate(parametersList[1:]): 
+        if(len(np.unique(interestConfigurations[:,j])) == 1):
+            title += name + " = " + str(interestConfigurations[0,j]) 
+            if(parametersUnits[1:][j] != ""):
+                title += " (" + parametersUnits[1:][j] + ")"
+            title += " "
+    title = title[:-1]
+
     for i,configuration in enumerate(interestConfigurations):
         label = ""
         for j,name in enumerate(parametersList[1:]):
             if(len(np.unique(interestConfigurations[:,j])) > 1):
-                label += name + " = " + str(configuration[j]) + "\n"
+                label += name + " = " + str(configuration[j]) 
+                if(parametersUnits[1:][j] != ""):
+                    label += " (" + parametersUnits[1:][j] + ")"
+                label += "\n"
         label = label[:-1]
         axLin.plot(parameterValues,plotList[i],label=label,color=cmap(i))
         axLog.plot(parameterValues,np.log10(plotList[i]),label=label,color=cmap(i))
 
-    axLog.set_xlabel(parameter)
+    axLog.set_xlabel(parameter + " (" + parametersUnits[0] + ")")
     axLog.set_ylabel("log(Average error)")
     axLog.set_xscale('log')  
-    axLog.set_title("log(Average error)")    
+    axLog.set_title(title)    
 
     axLog.legend()
 
-    axLin.set_xlabel(parameter)
+    axLin.set_xlabel(parameter + " (" + parametersUnits[0] + ")")
     axLin.set_ylabel("Average error")
-    axLin.set_title("Average error")   
+    axLin.set_title(title)   
 
     axLin.legend()
 

post_processing/plotSphericPlaneCut.py
@@ -12,21 +12,34 @@ postProcessingFunctions["db"] = lambda Re,Im : 20*np.log10(np.abs(np.complex(Re,
 postProcessingFunctions["abs"] = lambda Re,Im : np.abs(np.complex(Re,Im))
 postProcessingFunctions["id"] = lambda Re,Im : np.complex(Re,Im)
 
-def plotSphericCut(postProcessingFunction = "dB"):
+def plotSphericCut(postProcessingFunction):
 
     #Get all output files
     fileList = np.array(glob.glob("output*"))
     parametersList = []
 
     #Get all parameters names, or define them
+    parametersList = []
+    parametersUnits = []
+
     try:
         with open("parameters.txt") as parametersFile:
-            parametersList = parametersFile.read().splitlines()
+            lines = parametersFile.read().splitlines()
+            for line in lines:
+                try:
+                    name,unit = line.split(' ')
+                except:
+                    name = line
+                    unit=""
+                parametersList.append(name)
+                parametersUnits.append(unit)
     except:
         for i in range(len(fileList[0].split("_")) - 1):
             parametersList.append("parameter"+str(i+1))
+            parametersUnits.append("")
 
     parametersList = np.array(parametersList)
+    parametersUnits = np.array(parametersUnits)
 
     #Create files/parameters matrix
     P = np.empty((len(fileList),len(parametersList)))
@@ -41,9 +54,9 @@ def plotSphericCut(postProcessingFunction = "dB"):
     for i,parameter in enumerate(parametersList):
         if(len(np.unique(subP[:,0])) == 1):
             value = str(subP[0,0]) if subP[0,0] != int(subP[0,0]) else str(int(subP[0,0]))
-            print("Only possible for " + parameter +" is " + value)
+            print("Only possible for " + parameter +" is " + value +  " (" + parametersUnits[i] + ")")
         else:
-            value = input("What value for " + parameter + " ? " + str(np.unique(subP[:,0])))
+            value = input("What value for " + parameter + " ? " + str(np.unique(subP[:,0])) + " (" + parametersUnits[i] + ")")
             value = value if float(value) != int(float(value)) else str(int(float(value)))
 
         configuration.append(value)
@@ -79,6 +92,11 @@ def plotSphericCut(postProcessingFunction = "dB"):
     #AmpSomme = np.roll(AmpSomme,X0)
     AmpSomme = np.append(AmpSomme,AmpSomme[0])
 
+    #Removing infinite values
+    IndexInf = np.argwhere(np.isinf(AmpAnalytique))
+    AmpAnalytique = np.delete(AmpAnalytique,IndexInf)
+    AmpSomme = np.delete(AmpSomme,IndexInf)
+
     TH = [0]
     counterTh = 0
 
@@ -91,16 +109,20 @@ def plotSphericCut(postProcessingFunction = "dB"):
         counterTh += np.arccos(np.dot(u,v)/(np.linalg.norm(u)*np.linalg.norm(v)))
         TH.append(counterTh)
 
+    TH = np.delete(TH,IndexInf)
+
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
     ax.plot(TH,AmpAnalytique,label="Analytical solution (dB)")
     ax.plot(TH,AmpSomme,label="Numerical solution (dB)")
 
-    label = "Acoustic pressure field computed for \n"
+    print("Error = " + str(np.sqrt(np.average((AmpSomme - AmpAnalytique)**2))))
+
+    label = "Acoustic pressure field computed for : \n"
     for j,name in enumerate(parametersList):
-        label += name + " = " + configuration[j] + "\n"
+        label += name + " = " + configuration[j] + " (" + parametersUnits[j] + ") "
     label = label[:-1]
 
-    ax.set_title(label, x=-0.25, y=0.25)
+    ax.set_title(label)
 
     maxAmp = max(max(AmpSomme),max(AmpAnalytique))*1.1
 