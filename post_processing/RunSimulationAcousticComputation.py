Fixing computation counter

post_processing/RunSimulationAcousticComputation.py
@@ -39,11 +39,11 @@ Frequencies = [100,250,500,750,1000,2500,5000]
 Radius = [0.1*verificationRadius,0.25*verificationRadius,0.5*verificationRadius]
 Resolutions = [0.01*verificationRadius,0.025*verificationRadius,0.05*verificationRadius,0.075*verificationRadius] 
 
-SigmasPosition = [0.0025*verificationRadius,0.005*verificationRadius,0.01*verificationRadius]   
+SigmasPosition = [0.0025*verificationRadius,0.005*verificationRadius,0.01*verificationRadius]
 SigmasMeasure = [0.0]
 Nsigma = 10
 
-parametersCombinations = len(Frequencies)*len(Radius)*len(Resolutions)*max(1,len(np.nonzero(SigmasPosition)[0]))*max(1,len(np.nonzero(SigmasMeasure)[0]))*Nsigma
+parametersCombinations = len(Frequencies)*len(Radius)*len(Resolutions)*max(1,Nsigma*len(np.nonzero(SigmasPosition)[0]))*max(1,Nsigma*len(np.nonzero(SigmasMeasure)[0]))
 
 if(__name__ == "__main__"):
     counter = 1