e16dfc38210ecb09151ce6d0fb9b3ec044926342
CarolinePascal <caroline8.pascal@gmail.com>
2023-10-17 18:46:38 +0200

    Unifying computation calls

--- post_processing/RunMeasurementsAcousticComputation.py
+++ post_processing/RunMeasurementsAcousticComputation.py
@@ -4,12 +4,21 @@
 import subprocess
 import os
 import glob
+import sys
 
-#Utility packages
-import numpy as np
+### Studied method
+method = "BEM"
+try:
+    method = sys.argv[1]
+except:
+    print("Invalid size and resolution, switching to default : " + method)
+
+if(method == "BEM"):
+    command = "ff-mpirun -np 4 AcousticComputationBEM.edp -wg"
+elif(method == "SFT"):
+    command = "FreeFem++ AcousticComputationSFT.edp"
 
 ### Studied parameters
-cutRadius = 0.25
 Frequencies = sorted([int(os.path.basename(file).split(".")[0].split("_")[-1]) for file in glob.glob("./data*.csv")])
 
 if(__name__ == "__main__"):
@@ -19,8 +28,15 @@ if(__name__ == "__main__"):
 
         ### Run computation
 
-        bashCommand = "ff-mpirun -np 4 AcousticComputationBEM.edp -frequency " + str(frequency) + " -realMeasurements 1 -ns"
+        bashCommand = command + " -frequency " + str(frequency) + " -realMeasurements 1 -ns"
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+        #DEBUG
+        print(output.decode())
+
+        killProcess = "killall FreeFem++-mpi"
+        process = subprocess.Popen(killProcess.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
         #DEBUG
