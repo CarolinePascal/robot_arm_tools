Fixing mesh and point cloud plot functions, fixing mesh center computation

post_processing/measurements/DataProcessingMeasurements.py
@@ -21,8 +21,26 @@ import matplotlib.pyplot as plt
 
 from DataProcessingTools import plot_3d_data, save_fig, set_title, fmin, fmax, octBand, figsize, octBandFrequencies
 
-sys.path.append(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))) + "/scripts")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname((os.path.abspath(__file__))))) + "/scripts")
 from MeshTools import plotMesh, plotPointCloudFromPath
+
+def sphereFit(points):
+    A = np.zeros((len(points),4))
+    A[:,0] = points[:,0]*2
+    A[:,1] = points[:,1]*2
+    A[:,2] = points[:,2]*2
+    A[:,3] = 1
+
+    #   Assemble the f matrix
+    f = np.zeros((len(points),1))
+    f[:,0] = (points[:,0]**2) + (points[:,1]**2) + (points[:,2]**2)
+    output, residules, rank, singval = np.linalg.lstsq(A,f)
+
+    #   solve for the radius
+    radius = np.sqrt(output[0]**2 + output[1]**2 + output[2]**2 + output[3])
+    center = output[:3].T
+
+    return(center, radius)
 
 if __name__ == "__main__":
 
@@ -116,7 +134,7 @@ if __name__ == "__main__":
 			TFE = P.tfe_farina([fmin,fmax])
 		else:
 			TFE = P.tfe_welch(V) #Also possible for dB values : (P*V.rms).tfe_welch(V)
-   
+	
 		for j,f in enumerate(Frequencies):
 			Data[j,i] = TFE.nth_oct_smooth_to_weight_complex(octBand,fmin=f,fmax=f).acomplex[0]
 
@@ -136,8 +154,11 @@ if __name__ == "__main__":
 	Z = np.array(Z)
 
 	Points = np.array([X,Y,Z]).T
-	#Centroid = np.array([0.4419291797546691,-0.012440529880238332,0.5316684442730065])
-	Centroid = np.mean(Points,axis=0)
+
+	#To adapt depending on the mesh type ! 
+	#centroid = np.array([0.4419291797546691,-0.012440529880238332,0.5316684442730065])
+	#centroid = np.mean(Points,axis=0)
+	centroid, _ = sphereFit(Points)
 
 	if(meshPath is None):
 
@@ -181,8 +202,8 @@ if __name__ == "__main__":
 		resolution = np.mean(detailedMesh.edges_unique_length)
 		Centroids = detailedMesh.triangles_center
 
-		MeasurementsVertices = Vertices + Centroid
-		MeasurementsCentroids = Centroids + Centroid
+		MeasurementsVertices = Vertices + centroid
+		MeasurementsCentroids = Centroids + centroid
   
 		if(elementType == "P0"):
 			MeasurementsPoints = MeasurementsCentroids
@@ -201,6 +222,7 @@ if __name__ == "__main__":
 
 				#If the closest point is too far away, we consider that the Measurements point is missing
 				if(distances[indexMin] > resolution/2):
+						print("Missing measurement detected at point " + str(MeasurementsPoint) + " (" + str(distances[indexMin]) + " m)")
 						missing.append(i)
 				else:
 						OrderedData[:,i] = Data[:,indexMin]
@@ -215,16 +237,16 @@ if __name__ == "__main__":
 
 			figAmp,axAmp = plt.subplots(1,figsize=figsize,subplot_kw=dict(projection='3d'))
 			figPhase,axPhase = plt.subplots(1,figsize=figsize,subplot_kw=dict(projection='3d'))
-			plot_3d_data(np.abs(data), MeasurementsPoints.T, axAmp, pointCloudPath, label = r"$|$H$|$ (Pa/V)")
-			plot_3d_data(wrap(np.angle(data)), MeasurementsPoints.T, axPhase, pointCloudPath, label = "Phase (rad)")
+			plot_3d_data(np.abs(data), MeasurementsPoints.T, axAmp, label = r"$|$H$|$ (Pa/V)")
+			plot_3d_data(wrap(np.angle(data)), MeasurementsPoints.T, axPhase, label = "Phase (rad)")
 
 			if(pointCloudPath is not None):
 				plotPointCloudFromPath(pointCloudPath, axAmp)
 				plotPointCloudFromPath(pointCloudPath, axPhase)
 
 			if(meshPath is not None):
-				plotMesh(meshPath, axAmp)
-				plotMesh(meshPath, axPhase)
+				plotMesh(MeasurementsVertices, Faces, ax = axAmp)
+				plotMesh(MeasurementsVertices, Faces, ax = axPhase)
 
 			set_title(axAmp,"Pressure/Input signal TFE amplitude at " + str(int(f)) + " Hz\n1/" + str(octBand) + " octave smoothing")
 			set_title(axPhase,"Pressure/Input signal TFE phase at " + str(int(f)) + " Hz\n1/" + str(octBand) + " octave smoothing")

scripts/MeshTools.py
@@ -322,53 +322,61 @@ def generateCircularMesh(size, resolution, elementType = "P1", saveMesh = False,
 #  @param elementType Type of element
 #  @param plotEdges Wether to plot the edges or not
 #  @param plotNodes Wether to plot the elements nodes or not
-#  @param axes Matplotlib axes
-def plotMesh(vertices, faces, elementType = "P0", plotEdges = True, plotNodes = True, axes = None, **kwargs):
+#  @param ax Matplotlib axes
+def plotMesh(vertices, faces, elementType = "P0", plotEdges = True, plotNodes = False, ax = None, **kwargs):
 
-    if(axes is None):
-        _,axes = plt.subplots(1,subplot_kw=dict(projection='3d'))
+    if(ax is None):
+        _,ax = plt.subplots(1,subplot_kw=dict(projection='3d'))
 
     if(plotEdges):
         kwargsEdges = deepcopy(kwargs)
-        kwargsEdges.facecolor = (0,0,0,0)
+        kwargsEdges["facecolor"] = (0,0,0,0)
+        if(not "edgecolor" in kwargsEdges):
+            kwargsEdges["edgecolor"] = (0,0,0,0.1)
         plotMesh = art3d.Poly3DCollection(vertices[faces], **kwargsEdges)
-        axes.add_collection3d(copy(plotMesh))
+        ax.add_collection3d(copy(plotMesh))
 
     if(plotNodes):
         kwargsNodes = deepcopy(kwargs)
-        kwargsNodes.marker = "o"
+        kwargsNodes["marker"] = "o"
         if (elementType == "P0"):
             centroids = np.average(vertices[faces],axis=1)
-            axes.scatter(centroids[:,0],centroids[:,1],centroids[:,2], **kwargsNodes)
+            ax.scatter(centroids[:,0],centroids[:,1],centroids[:,2], **kwargsNodes)
         elif (elementType == "P1"):
-            axes.scatter(vertices[:,0],vertices[:,1],vertices[:,2], **kwargsNodes)
+            ax.scatter(vertices[:,0],vertices[:,1],vertices[:,2], **kwargsNodes)
         else:
             print("Invalid element type, nodes will not be displayed")
 
-    return(axes)
+    return(ax)
 
-def plotMeshFromPath(meshPath, elementType = "P0", plotEdges = True, plotNodes = True, axes = None, **kwargs):
+def plotMeshFromPath(meshPath, elementType = "P0", plotEdges = True, plotNodes = False, ax = None, **kwargs):
 	mesh = meshio.read(meshPath)
 	vertices, faces = mesh.points, mesh.get_cells_type("triangle")
-	return(plotMesh(vertices, faces, elementType, plotEdges, plotNodes, axes, **kwargs))
+	return(plotMesh(vertices, faces, elementType, plotEdges, plotNodes, ax, **kwargs))
 
 ## Function plotting a colored point cloud
 #  @param points Point cloud points
 #  @param colors Point cloud points colors
-#  @param axes Matplotlib axes
-def plotPointCloud(points, colors = None, axes = None, **kwargs):
-    if(axes is None):
-        _,axes = plt.subplots(1,subplot_kw=dict(projection='3d'))
-        
-    axes.scatter(*points, c = colors, **kwargs)
-    return(axes)
+#  @param ax Matplotlib axes
+def plotPointCloud(points, colors = None, ax = None, **kwargs):
+    if(ax is None):
+        _,ax = plt.subplots(1,subplot_kw=dict(projection='3d'))
 
-def plotPointCloudFromPath(pointCloudPath, axes = None, **kwargs):
+    kwargsPointCloud = deepcopy(kwargs)
+    kwargsPointCloud["c"] = colors
+
+    if(not "s" in kwargsPointCloud):
+        kwargsPointCloud["s"] = 2
+
+    ax.scatter(*points.T, **kwargsPointCloud)
+    return(ax)
+
+def plotPointCloudFromPath(pointCloudPath, ax = None, **kwargs):
     pointCloud = o3d.io.read_point_cloud(pointCloudPath) 
     pointCloud = pointCloud.voxel_down_sample(voxel_size=0.005)
     points = np.array(pointCloud.points)
     colors = np.array(pointCloud.colors)
-    return(plotPointCloud(points, colors, axes, **kwargs))
+    return(plotPointCloud(points, colors, ax, **kwargs))
 
 ## Function displaying mesh information
 #  @param vertices Mesh vertices