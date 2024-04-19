4ed205e555d913f32106a98c71c6dd46630354aa
CarolinePascal <caroline8.pascal@gmail.com>
2024-04-19 19:38:47 +0200

    Adding proper python installation for measurements processing tools

---
+++ post_processing/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='robot_arm_acoustic_post_processing',
+   version='0.0.0',
+   description='The robot_arm_acoustic_post_processing package',
+   author='Caroline PASCAL',
+   author_email='caroline.pascal.2020@ensta-paris.fr',
+   packages=['robot_arm_acoustic_post_processing'], 
+   package_dir={'': '.'},
+   install_requires=[
+   ]
+)
