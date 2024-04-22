20747a5258881efa31fd936a118eb75dfa712093
Caroline Pascal <caroline.pascal.2020@ensta-paris.fr>
2024-04-22 20:07:41 +0200

    Update setup.py

--- post_processing/setup.py
+++ post_processing/setup.py
@@ -13,15 +13,19 @@ setup(
                 'robot_arm_acoustic_post_processing.measurements': './robot_arm_acoustic_post_processing',
                 'robot_arm_acoustic_post_processing.simulations': './robot_arm_acoustic_post_processing'},
    install_requires=[
-      "measpy",
+      #Acoustic packages
+      "unyt",
+      "csaps",
+      "measpy==0.0.15",
       "numpy",
+      #Plot packages
       "matplotlib",
       "plotly",
+      #Mesh packages
       "meshio",
       "trimesh",
       "open3d",
-      "unyt",
-      "csaps",
+      #CLI package
       "cloup",
    ]
 )
