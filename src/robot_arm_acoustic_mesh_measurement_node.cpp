90ae25b969f7282455f47991dbaec8fc8bfe4d0d
pascal.2020 <caroline.pascal.2020@ensta-paris.fr>
2024-06-11 15:06:39 +0200

    Updating mesh measurements to generic meshes

--- src/robot_arm_acoustic_mesh_measurement_node.cpp
+++ src/robot_arm_acoustic_mesh_measurement_node.cpp
@@ -16,59 +16,12 @@ int main(int argc, char **argv)
 
     //Robot initialisation
     Robot robot;
-    RobotVisualTools robotVisualTools;
-    
-    //Get studied object pose
-    ros::NodeHandle n;
-    std::vector<double> objectPoseArray;
-    
-    if(!n.getParam("objectPose",objectPoseArray))
-    {
-        ROS_ERROR("Unable to retrieve studied object pose !");
-        throw std::runtime_error("MISSING PARAMETER");
-    }
-
-    tf2::Quaternion quaternion;
-    quaternion.setRPY(objectPoseArray[3],objectPoseArray[4],objectPoseArray[5]);
-
-    geometry_msgs::Pose objectPose;
-    objectPose.position.x = objectPoseArray[0];
-    objectPose.position.y = objectPoseArray[1];
-    objectPose.position.z = objectPoseArray[2];
-    objectPose.orientation =  tf2::toMsg(quaternion);
-
-    double meshSize, objectSize;
-    std::string meshType;
-    if(!n.getParam("objectSize",objectSize))
-    {
-        ROS_ERROR("Unable to retrieve studied object size !");
-        throw std::runtime_error("MISSING PARAMETER");
-    }
-    if(!n.getParam("meshSize",meshSize))
-    {
-        ROS_ERROR("Unable to retrieve mesh size !");
-        throw std::runtime_error("MISSING PARAMETER");
-    }
-    if(!n.getParam("meshType",meshType))
-    {
-        ROS_ERROR("Unable to retrieve mesh type !");
-        throw std::runtime_error("MISSING PARAMETER");
-    }
-
-    if(objectSize > meshSize)
-    {
-        ROS_WARN("Mesh size below object size : some poses might not be reachable !");
-    }
-
-    if(meshType=="sphere")
-    {
-        robotVisualTools.addSphere("meshSphere",objectPose,meshSize/2,true);
-    }
 
     //Create measurement waypoints poses
     std::vector<geometry_msgs::Pose> waypoints;
 
     //Get mesh file path, or generate mesh and get its file path
+    ros::NodeHandle n;
     std::string meshPath;
     if(!n.getParam("meshPath",meshPath))
     {
@@ -90,11 +43,15 @@ int main(int argc, char **argv)
             ros::WallDuration(2.0).sleep();
         }
     }
-    
-    translateTrajectory(waypoints, objectPose.position.x, objectPose.position.y, objectPose.position.z);
+    if(waypoints.empty())
+    {
+        ROS_ERROR("Unable to retrieve mesh waypoints !");
+        throw std::runtime_error("MISSING PARAMETER");
+    }
+
 
     //Main loop
-    robot.runMeasurementRoutine(waypoints,false,true,-1);
+    robot.runMeasurementRoutine(waypoints,false,true,-1,true,false);
 
     //Shut down ROS node 
     ros::shutdown();
